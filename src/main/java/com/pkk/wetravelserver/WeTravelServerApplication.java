cee53190af0205a1c4919ffc01a9a4c35773186a
artem.nishchenko0@gmail.com <hk3pmpumvy>
2022-11-09 20:19:07 +0200

    Initial commit

---
+++ src/main/java/com/pkk/wetravelserver/WeTravelServerApplication.java
@@ -0,0 +1,13 @@
+package com.pkk.wetravelserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeTravelServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeTravelServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pkk/wetravelserver/WeTravelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkk.wetravelserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeTravelServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
